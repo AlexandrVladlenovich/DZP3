014ad7792182ddf9854f5e30b769bad13edfb905
AlexandrVladlenovich <110374871+AlexandrVladlenovich@users.noreply.github.com>
2022-10-04 23:55:50 +0500

    add Zadacha5_Fibonnachi.py

---
+++ Zadacha5_Fibonnachi.py
@@ -0,0 +1,13 @@
+n = int(input('Задайте число: '))
+if n < 0: n = n*-1
+f1 = f2 = 1
+list1 = [f1, f2]
+for i in range(2, n):
+    f1,f2 = f2, f1 + f2
+    list1.append(f2)
+print('Список Фибоначчи:', list1)
+f1=f2=1
+for i in range(-n, 1):
+    f1,f2 = f2, f1 - f2
+    list1.insert(0, f2)
+print('Список Негафибоначчи:', list1)
